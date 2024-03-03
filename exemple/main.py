f9e9d7d6b771af0d44cc215ccbaa5c0e6097024c
bydlo <rollurioari@gmail.com>
2024-03-03 20:14:20 +0500

    2commit

--- exemple/main.py
+++ exemple/main.py
@@ -1,1 +1,14 @@
-print('a')
+from tkinter import *
+from tkinter import ttk
+import json
+root = Tk()
+
+with open('colorJson.json','r') as colors:
+    colorMap = json.load(colors)
+
+root.geometry("500x500")
+print(colorMap)
+
+#mainFrame = ttk.Frame(width=700,height=300,bg = colorMap)
+
+root.mainloop()
